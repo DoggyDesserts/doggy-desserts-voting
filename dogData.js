c220f396cd0617467b08d33425ccd42d8dfdebf0
DoggyDesserts <hello@doggydesserts.co.uk>
2025-04-24 21:04:56 +0100

    Add files via upload

---
+++ dogData.js
@@ -0,0 +1,72 @@
+const dogData = [
+  {
+    "id": 1,
+    "name": "Alita",
+    "image": "https://static.wixstatic.com/media/3342f4_46bbfed100b447339cb9c08dc03f3d36~mv2.jpg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.216Z"
+  },
+  {
+    "id": 2,
+    "name": "Angel",
+    "image": "https://static.wixstatic.com/media/3342f4_b11d2334da5c4f77b4d1bf93b758e3b7~mv2.jpg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 3,
+    "name": "Arlo",
+    "image": "https://static.wixstatic.com/media/3342f4_e502f8d0d97a4c2480a6c294acde95eb~mv2.jpg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 4,
+    "name": "Bailey",
+    "image": "https://static.wixstatic.com/media/3342f4_e1315c4e2bdc4436847bef738a3270ee~mv2.jpg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 5,
+    "name": "Barney",
+    "image": "https://static.wixstatic.com/media/3342f4_c54bfc7a2d044d339535768d394490af~mv2.jpg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 6,
+    "name": "Bella",
+    "image": "https://static.wixstatic.com/media/3342f4_b85bf52d7d8b4c888587dd7a93b43d85~mv2.jpeg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 7,
+    "name": "Benni",
+    "image": "https://static.wixstatic.com/media/3342f4_78680df7184f40e29815774596772521~mv2.jpeg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 8,
+    "name": "Billy",
+    "image": "https://static.wixstatic.com/media/3342f4_47851e6f9183491394a2c7bbeedbe4d4~mv2.jpeg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 9,
+    "name": "Buddy",
+    "image": "https://static.wixstatic.com/media/3342f4_6610ddc338954602903915f7511d14aa~mv2.jpg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  },
+  {
+    "id": 10,
+    "name": "Buddy",
+    "image": "https://static.wixstatic.com/media/3342f4_54b481c2a5bc4fc686543914403b8a78~mv2.jpg",
+    "votes": 0,
+    "date": "2025-04-24T19:57:02.219Z"
+  }
+];
